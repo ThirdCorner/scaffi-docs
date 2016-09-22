7fdcdcd47e11962028d548971769ff31ee55b9d5
Joseph Blake <jblake@thirdcorner.com>
2016-09-22 14:40:50 -0500

    Testing without failed test

--- src/ui/app/routes/index/index.page.spec.js
+++ src/ui/app/routes/index/index.page.spec.js
@@ -11,9 +11,7 @@ describe("Test Form", ()=>{
 		it("should have browsed", ()=>{
 			expect(element(by.id("logo")).isPresent()).to.eventually.be.true;
 		})
-		it("should have browsed", ()=>{
-			expect(element(by.id("spazzy")).isPresent()).to.eventually.be.true;
-		})
+	
 		
 	});
 
